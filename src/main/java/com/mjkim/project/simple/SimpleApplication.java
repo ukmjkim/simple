77e43b37b76f06e463660cb89f7b178a776ebd07
ukmjkim <ukmjkim@gmail.com>
2023-05-05 20:14:27 -0700

    Update SimpleApplication.java

--- src/main/java/com/mjkim/project/simple/SimpleApplication.java
+++ src/main/java/com/mjkim/project/simple/SimpleApplication.java
@@ -3,19 +3,13 @@ package com.mjkim.project.simple;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SimpleApplication {
 
-	public static void main(String[] args) {
-		final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-		final LocalDateTime now = LocalDateTime.now();
-		
-		System.out.println("Hello, World! The current time is " + dtf.format(now));
+  public static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
 
-		SpringApplication.run(SimpleApplication.class, args);
-	}
+    System.out.println("Hello, World! The current time is " + dtf.format(now));
+  }
 
 }
