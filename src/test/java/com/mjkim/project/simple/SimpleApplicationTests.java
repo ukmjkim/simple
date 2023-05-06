dbb5331c7858b945256e7dad0461dbd03da6d911
ukmjkim <ukmjkim@gmail.com>
2023-05-05 20:14:06 -0700

    Update SimpleApplicationTests.java

--- src/test/java/com/mjkim/project/simple/SimpleApplicationTests.java
+++ src/test/java/com/mjkim/project/simple/SimpleApplicationTests.java
@@ -1,12 +1,8 @@
 package com.mjkim.project.simple;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SimpleApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
